5dac152b8777a7119153e4c9edf844e1af3903ed
Ed Burtnieks <edburtnieks@gmail.com>
2019-10-02 21:17:01 +0300

    Add side navigation to styleguide page - #21

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
   import SpecialOffers from "./pages/SpecialOffers/index.svelte";
   import News from "./pages/News/index.svelte";
   import Contacts from "./pages/Contacts/index.svelte";
-  import Styleguide from "./pages/Styleguide/index.svelte";
+  import StyleguideInputs from "./pages/Styleguide/Inputs/index.svelte";
   import Header from "./components/Header/index.svelte";
   import Footer from "./components/Footer/index.svelte";
 </script>
@@ -18,7 +18,7 @@
   <Route path="special-offers" component={SpecialOffers} />
   <Route path="news" component={News} />
   <Route path="contacts" component={Contacts} />
-  <Route path="styleguide" component={Styleguide} />
+  <Route path="styleguide-inputs" component={StyleguideInputs} />
 
   <Footer />
 </Router>
